c7619c62b9a032fd68336921e8d4ee8e90e4339c
bsh <qortmdgh0320@gmail.com>
2026-04-21 09:54:21 +0900

    라이브러리 삭제

--- app/_7/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "pularchive · 샘플 6",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
